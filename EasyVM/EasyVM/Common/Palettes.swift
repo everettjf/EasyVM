1239f732e5fe6c529611d624e495f7696c5fd2e3
everettjf <everettjf@live.com>
2022-08-27 23:47:33 +0800

    fix

---
+++ EasyVM/EasyVM/Common/Palettes.swift
@@ -0,0 +1,22 @@
+//
+//  Palettes.swift
+//  EasyVM
+//
+//  Created by everettjf on 2022/8/27.
+//
+
+import Foundation
+import SwiftUI
+
+let globalPalettes: [Color] = [
+    Color(red: 0.73, green: 0.20, blue: 0.20),
+    Color(red: 0.95, green: 0.66, blue: 0.24),
+    Color(red: 0.14, green: 0.29, blue: 0.49),
+    Color(red: 0.46, green: 0.76, blue: 0.67),
+    Color(red: 0.30, green: 0.33, blue: 0.22),
+    Color(red: 0.49, green: 0.55, blue: 0.64),
+    Color(red: 0.92, green: 0.53, blue: 0.30),
+    Color(red: 0.20, green: 0.45, blue: 0.55),
+    Color(red: 0.41, green: 0.45, blue: 0.45),
+    Color(red: 0.87, green: 0.67, blue: 0.61)
+]
--- EasyVM/EasyVM/Main/ContentView.swift
+++ EasyVM/EasyVM/Main/ContentView.swift
@@ -23,23 +23,23 @@ enum PartyTask: String, Identifiable, CaseIterable, Hashable {
     var color: Color {
         switch self {
         case .food:
-            return palette[0]
+            return globalPalettes[0]
         case .supplies:
-            return palette[1]
+            return globalPalettes[1]
         case .invitations:
-            return palette[2]
+            return globalPalettes[2]
         case .eventDetails:
-            return palette[3]
+            return globalPalettes[3]
         case .funProjection:
-            return palette[4]
+            return globalPalettes[4]
         case .activities:
-            return palette[5]
+            return globalPalettes[5]
         case .vips:
-            return palette[6]
+            return globalPalettes[6]
         case .music:
-            return palette[7]
+            return globalPalettes[7]
         case .photosFilter:
-            return palette[8]
+            return globalPalettes[8]
         }
     }
     
@@ -115,18 +115,6 @@ enum PartyTask: String, Identifiable, CaseIterable, Hashable {
     }
 }
 
-private let palette: [Color] = [
-    Color(red: 0.73, green: 0.20, blue: 0.20),
-    Color(red: 0.95, green: 0.66, blue: 0.24),
-    Color(red: 0.14, green: 0.29, blue: 0.49),
-    Color(red: 0.46, green: 0.76, blue: 0.67),
-    Color(red: 0.30, green: 0.33, blue: 0.22),
-    Color(red: 0.49, green: 0.55, blue: 0.64),
-    Color(red: 0.92, green: 0.53, blue: 0.30),
-    Color(red: 0.20, green: 0.45, blue: 0.55),
-    Color(red: 0.41, green: 0.45, blue: 0.45),
-    Color(red: 0.87, green: 0.67, blue: 0.61)
-]
 
 
 struct TaskLabel: View {
