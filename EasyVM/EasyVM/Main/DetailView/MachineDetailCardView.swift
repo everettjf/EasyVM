a53982fba68e75004177d737862ffba1bb12a570
everettjf <everettjf@live.com>
2022-09-04 21:54:15 +0800

    basic ui ready 🌈

---
+++ EasyVM/EasyVM/Main/DetailView/MachineDetailCardView.swift
@@ -0,0 +1,98 @@
+//
+//  MachineDetailCardView.swift
+//  EasyVM
+//
+//  Created by everettjf on 2022/9/4.
+//
+
+import SwiftUI
+
+
+struct MachineDetailCardView: View {
+    
+    let vm: MyVM
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 6) {
+            HStack {
+                Text(vm.name)
+                    .font(.headline)
+                Spacer()
+                Text(vm.osType == .linux ? "Linux" : "macOS")
+                    .font(.footnote)
+                    .fontWeight(.bold)
+            }
+            
+            Rectangle()
+                .foregroundColor(vm.color)
+                .frame(height:120)
+                .cornerRadius(5)
+            
+
+            Group {
+                HStack(spacing: 2) {
+                    Image(systemName: "doc.viewfinder.fill")
+                    Text("Disk: \(vm.disk)")
+                }
+                .font(.caption2)
+                HStack(spacing: 2) {
+                    Image(systemName: "cpu")
+                    Text("CPU: \(vm.cpu)")
+                }
+                .font(.caption2)
+                HStack(spacing: 2) {
+                    Image(systemName: "memorychip")
+                    Text("Memory: \(vm.memory)")
+                }
+                .font(.caption2)
+                
+                HStack(alignment: .top, spacing: 2) {
+                    Image(systemName: "circle.hexagonpath")
+                    Text("Attributes: \(vm.attributes)")
+                        .lineLimit(3)
+                }
+                .font(.caption2)
+                
+                HStack(alignment: .top, spacing: 2) {
+                    Image(systemName: "captions.bubble")
+                    Text("Description: \(vm.remark)")
+                        .lineLimit(3)
+                }
+                .font(.caption2)
+            }
+            
+            HStack {
+                Spacer()
+                Button {
+                    
+                } label: {
+                    Image(systemName: "slider.vertical.3")
+                }
+                .buttonStyle(.borderless)
+
+            }
+        }
+        .padding(.all, 10)
+        .overlay(
+            RoundedRectangle(cornerRadius: 16)
+                .stroke(.blue, lineWidth: 1)
+        )
+        .shadow(radius: 16)
+        .frame(width: 230, height: 330)
+    }
+    
+}
+
+struct MachineDetailCardView_Previews: PreviewProvider {
+    static let width: CGFloat = 500
+    
+    static var previews: some View {
+        MachineDetailCardView(vm: MyVM(osType: .macOS, name: "My first macOS", color: .indigo, remark: "This is my first macOS virutal machine created from EasyVM from EasyVM which is super easy", disk: "64GB", cpu: 1, memory: "4GB", attributes: "NAT Network, from EasyVM from EasyVM Input/Output Audio Stream, ..."))
+            .preferredColorScheme(.dark)
+            .frame(width: width, height: width)
+        
+        MachineDetailCardView(vm: MyVM(osType: .macOS, name: "My first macOS", color: .pink, remark: "This is my first macOS virutal", disk: "64GB", cpu: 1, memory: "4GB", attributes: "NAT Network, Input/Output Audio Stream, ..."))
+            .preferredColorScheme(.light)
+            .frame(width: width, height: width)
+    }
+}
--- EasyVM/EasyVM/Main/DetailView/MachinesDetailView.swift
+++ EasyVM/EasyVM/Main/DetailView/MachinesDetailView.swift
@@ -11,6 +11,7 @@ struct MyVM : Identifiable, Hashable {
     let id = UUID()
     let osType: VMModelOSType
     let name: String
+    let color: Color
     let remark: String
     let disk: String
     let cpu: Int
@@ -20,143 +21,56 @@ struct MyVM : Identifiable, Hashable {
 
 class MyVMStore: ObservableObject {
     @Published var vms: [MyVM] = [
-        MyVM(osType: .macOS, name: "My first macOS", remark: "This is my first macOS virutal machine created from EasyVM which is super easy", disk: "64GB", cpu: 1, memory: "4GB", attributes: "NAT Network, Input/Output Audio Stream, ..."),
+        MyVM(osType: .macOS, name: "My first macOS", color: .pink, remark: "This is my first macOS virutal machine created from EasyVM which is super easy", disk: "64GB", cpu: 1, memory: "4GB", attributes: "NAT Network, Input/Output Audio Stream, ..."),
         
-        MyVM(osType: .linux, name: "My first linux", remark: "This is my first linux virutal machine created from EasyVM which is super easy", disk: "64GB", cpu: 2, memory: "4GB", attributes: "NAT Network, Input/Output Audio Stream, ..."),
+        MyVM(osType: .linux, name: "My first linux", color: .blue, remark: "This is my first linux virutal machine created from EasyVM which is super easy", disk: "64GB", cpu: 2, memory: "4GB", attributes: "NAT Network, Input/Output Audio Stream, ..."),
         
-        MyVM(osType: .macOS, name: "Her first macOS", remark: "This is her first macOS virutal machine created from EasyVM which is super easy", disk: "32GB", cpu: 2, memory: "4GB", attributes: "NAT Network, Input/Output Audio Stream, ..."),
+        MyVM(osType: .macOS, name: "Her first macOS", color: .indigo, remark: "This is her first macOS virutal machine created from EasyVM which is super easy", disk: "32GB", cpu: 2, memory: "4GB", attributes: "NAT Network, Input/Output Audio Stream, ..."),
         
-        MyVM(osType: .macOS, name: "Her second macOS", remark: "This is her second macOS virutal machine created from EasyVM which is super easy", disk: "16GB", cpu: 1, memory: "4GB", attributes: "NAT Network, Input/Output Audio Stream, ..."),
+        MyVM(osType: .macOS, name: "Her second macOS", color: .yellow, remark: "This is her second macOS virutal machine created from EasyVM which is super easy", disk: "16GB", cpu: 1, memory: "4GB", attributes: "NAT Network, Input/Output Audio Stream, ..."),
         
-        MyVM(osType: .linux, name: "Her first linux", remark: "This is her first linux virutal machine created from EasyVM which is super easy", disk: "32GB", cpu: 2, memory: "6GB", attributes: "NAT Network, Input/Output Audio Stream, ..."),
+        MyVM(osType: .linux, name: "Her first linux", color: .green, remark: "This is her first linux virutal machine created from EasyVM which is super easy", disk: "32GB", cpu: 2, memory: "6GB", attributes: "NAT Network, Input/Output Audio Stream, ..."),
         
     ]
 }
 
 
-enum AttendanceScope {
-    case inPerson
-    case online
-}
-
-struct AttendeeToken: Identifiable, Equatable, Hashable {
-    enum Guts {
-        case name
-        case location
-        case status
-    }
-    
-    let guts: Guts
-    var query: String = .init()
-    
-    var id: String {
-        self.systemImage
-    }
-    
-    static let allCases: [AttendeeToken] = [.name, .location, .status]
-    
-    mutating func displayName(_ query: String) -> String {
-        self.query = query
-        switch guts {
-        case .name: return "Name contains: \(query)"
-        case .location: return "City contains: \(query)"
-        case .status: return "Status contains: \(query)"
-        }
-    }
-    
-    var systemImage: String {
-        switch guts {
-        case .name: return "person"
-        case .location: return "location.square"
-        case .status: return "person.crop.circle.badge"
-        }
-    }
-    
-    static let name: AttendeeToken = .init(guts: .name)
-    static let location: AttendeeToken = .init(guts: .location)
-    static let status: AttendeeToken = .init(guts: .status)
-}
 
 struct MachinesDetailView: View {
     @StateObject private var vmStore = MyVMStore()
-    @State private var selection = Set<MyVM.ID>()
     
-    // searchable
-    @State private var tokens: [AttendeeToken] = .init()
-    @State private var query: String = .init()
-    @State private var scope: AttendanceScope = .inPerson
+    let columns = [GridItem(.adaptive(minimum: 230, maximum: 230))]
     
-    
-    var table: some View {
-        Table(vmStore.vms, selection: $selection) {
-            TableColumn("OS") { vm in
-                Text("\(vm.osType.rawValue)")
-                    .frame(height: 60)
+    var grid: some View {
+        LazyVGrid(columns: columns, alignment: .listRowSeparatorLeading) {
+            ForEach(vmStore.vms) { item in
+                MachineDetailCardView(vm: item)
             }
-            .width(50)
-            
-            TableColumn("Name") { vm in
-                Text("\(vm.name)")
-            }
-            .width(min:150, ideal:150)
-            
-            TableColumn("Disk") { vm in
-                Text("\(vm.disk)")
-            }
-            .width(50)
-            
-            TableColumn("CPU Count") { vm in
-                Text("\(vm.cpu)")
-            }
-            .width(65)
-            
-            TableColumn("Memory") { vm in
-                Text("\(vm.memory)")
-            }
-            .width(50)
-            
-            TableColumn("Attributes") { vm in
-                Text("\(vm.attributes)")
-                    .lineLimit(nil)
-                    .multilineTextAlignment(.leading)
-            }
-            .width(min:100, ideal: 100)
-            
-            TableColumn("Description") { vm in
-                Text("\(vm.remark)")
-                    .lineLimit(nil)
-                    .multilineTextAlignment(.leading)
-            }
-            .width(min:100, ideal: 200)
-            
         }
     }
     
     var body: some View {
         NavigationStack {
-            table
-                .navigationTitle("Virtual Machines")
-                .contextMenu(forSelectionType: MyVM.ID.self, menu: { selection in
-                    if selection.isEmpty {
-                        Button("No selection") {  }
-                    } else if selection.count == 1 {
-                        Button("select 1") {  }
-                    } else {
-                        Button("Select > 1") {  }
-                    }
-                })
-                .toolbar(id: "toolbar") {
-                    ToolbarItem(id: "new", placement: .primaryAction) {
-                        Button(action: {}) {
-                            Label("New Virutal Machine", systemImage: "plus.diamond")
-                        }
-                    }
-                    ToolbarItem(id: "share", placement: .automatic) {
-                        Button(action: {}) {
-                            Label("Share", systemImage: "square.and.arrow.up")
-                        }
+            VStack {
+                ScrollView {
+                    grid
+                }
+            }
+            .padding(.leading, 5)
+            .padding(.trailing, 5)
+            .navigationTitle("Virtual Machines - EasyVM")
+            .toolbar(id: "toolbar") {
+                ToolbarItem(id: "new", placement: .primaryAction) {
+                    Button(action: {}) {
+                        Label("New Virutal Machine", systemImage: "plus.diamond")
                     }
                 }
+                ToolbarItem(id: "share", placement: .automatic) {
+                    Button(action: {}) {
+                        Label("Share", systemImage: "square.and.arrow.up")
+                    }
+                }
+            }
         }
         
     }
