d769a40635ad9b61d56dc1998f327c67de69cc95
everettjf <everettjf@live.com>
2022-09-14 09:44:26 +0800

    create phase

--- EasyVM/EasyVM/Create/CreatePhaseSystemImageView.swift
+++ EasyVM/EasyVM/Create/CreatePhaseSystemImageView.swift
@@ -8,8 +8,14 @@
 import SwiftUI
 
 struct CreatePhaseSystemImageView: View {
-    @State var imagePath: String = ""
+    enum ImageSource {
+        case latest_avaliable, input_url
+    }
     
+    
+    @State var imagePath: String = ""
+    @State var downloadMethod: ImageSource = .latest_avaliable
+    @State var downloadInputUrl: String = ""
     
     var body: some View {
         VStack {
@@ -47,12 +53,23 @@ struct CreatePhaseSystemImageView: View {
                 
                 Section("Or you can download system image directly from apple server") {
                     
-                    VStack(alignment: .leading) {
+                    VStack {
+                        
+                        Picker("Image Source", selection: $downloadMethod) {
+                            Text("Latest Avaliable Image").tag(ImageSource.latest_avaliable)
+                            Text("Image URL Input Below").tag(ImageSource.input_url)
+                        }
+                        .pickerStyle(.menu)
+                        
+                        if downloadMethod == .input_url {
+                            TextField("Image URL", text: $downloadInputUrl)
+                                .multilineTextAlignment(.trailing)
+                                .textFieldStyle(.plain)
+                        }
                         ProgressView(value: 250, total: 1000)
-                        Text("25% - 250MB / 1000GB").font(.caption)
-                        Text("Download not started").font(.caption)
                         
                         HStack {
+                            Text("25% - 250MB / 1000GB").font(.caption)
                             Spacer()
                             Button {
                                 
