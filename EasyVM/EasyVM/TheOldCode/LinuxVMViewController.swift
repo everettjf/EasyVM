8b1b3222fa725361ee798c49bdf235860cc97b2e
everettjf <everettjf@live.com>
2022-10-04 09:10:27 +0800

    edit

--- EasyVM/EasyVM/TheOldCode/LinuxVMViewController.swift
+++ EasyVM/EasyVM/TheOldCode/LinuxVMViewController.swift
@@ -9,6 +9,14 @@ import Cocoa
 import Foundation
 import Virtualization
 
+class LinuxPath {
+    static let vmBundlePath = NSHomeDirectory() + "/GUI Linux VM.bundle/"
+    static let mainDiskImagePath = vmBundlePath + "Disk.img"
+    static let efiVariableStorePath = vmBundlePath + "NVRAM"
+    static let machineIdentifierPath = vmBundlePath + "MachineIdentifier"
+}
+
+
 class LinuxVMViewController: NSViewController , VZVirtualMachineDelegate {
     private var virtualMachineView: VZVirtualMachineView!
 
